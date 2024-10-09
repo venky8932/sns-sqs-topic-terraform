13e6b21615cb3736139b0a297f32670d077bcccc
venky8932 <145984905+venky8932@users.noreply.github.com>
2024-10-09 13:00:43 +0530

    Update Sns.tf

--- Sns.tf
+++ Sns.tf
@@ -1,1 +1,16 @@
-
+stage('User Confirmation') {
+            steps {
+                script {
+                    // Pop-up asking for user confirmation to proceed or abort
+                    def userInput = input(
+                        id: 'ProceedOrAbort', message: 'Do you want to proceed with Terraform Apply/Destroy?', parameters: [
+                            [$class: 'BooleanParameterDefinition', defaultValue: true, description: 'Proceed with Terraform apply/destroy?', name: 'Proceed']
+                        ]
+                    )
+                    // Check the user's input
+                    if (userInput == false) {
+                        error "User chose to abort the operation. Pipeline will stop."
+                    }
+                }
+            }
+        }
